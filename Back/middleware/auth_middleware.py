16db9de5e3e0eadd93bee52007eab007ad6f32b7
yannaifischer <yannaif@highlander.io>
2024-07-20 19:28:03 +0300

    Added OPTIONS handling

--- Back/middleware/auth_middleware.py
+++ Back/middleware/auth_middleware.py
@@ -5,6 +5,9 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        if request.method == "OPTIONS":
+            return await call_next(request)
+
         headers = {key: value for key, value in request.headers.items()}
         response = requests.get("http://localhost:4000/api/auth/validateSession", headers=headers)
 
