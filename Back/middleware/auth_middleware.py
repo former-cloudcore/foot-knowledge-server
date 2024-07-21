479a10f0ac73aa514cbe1130e2bac6f6cfe61448
yannaifischer <yannaif@highlander.io>
2024-07-21 13:33:19 +0300

    Exported AUTH_SERVER_URL

--- Back/middleware/auth_middleware.py
+++ Back/middleware/auth_middleware.py
@@ -2,16 +2,15 @@ import requests
 from fastapi import Request, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 
+AUTH_SERVER_URL = "http://localhost:4000"
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        if request.method == "OPTIONS":
-            return await call_next(request)
+        if request.method != "OPTIONS":
+            headers = {key: value for key, value in request.headers.items()}
+            response = requests.get(f"{AUTH_SERVER_URL}/api/auth/validateSession", headers=headers)
 
-        headers = {key: value for key, value in request.headers.items()}
-        response = requests.get("http://localhost:4000/api/auth/validateSession", headers=headers)
-
-        if response.status_code != 200:
-            raise HTTPException(status_code=403, detail="Authentication failed")
+            if response.status_code != 200:
+                raise HTTPException(status_code=403, detail="Authentication failed")
 
         return await call_next(request)
