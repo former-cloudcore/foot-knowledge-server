8266ba26fd8102f11101c310037afac4b8570efa
jofjof <116435248+jofjof@users.noreply.github.com>
2024-07-27 11:13:36 +0300

    inetgrate scoreboard with connections game

--- Back/models/scoreboard.py
+++ Back/models/scoreboard.py
@@ -9,5 +9,6 @@ class ScoreModel(SQLModel):
     nickname: Mapped[str] = mapped_column("nickname")
     squares_number: Mapped[int] = mapped_column("squares_number")
     players_number: Mapped[int] = mapped_column("players_number")
+    shortest_path: Mapped[int] = mapped_column("shortest_path")
     time: Mapped[int] = mapped_column("time")
     board: Mapped[str] = mapped_column("board")
